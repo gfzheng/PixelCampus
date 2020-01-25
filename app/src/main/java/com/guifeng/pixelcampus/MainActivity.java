7adb0f1375b2cb719fb55550ce47ccd1b8afa5a2
郑贵锋 <gzheng@hotmail.com>
2020-01-25 18:56:47 +0800

    add shader and draw a color triangle

--- app/src/main/java/com/guifeng/pixelcampus/MainActivity.java
+++ app/src/main/java/com/guifeng/pixelcampus/MainActivity.java
@@ -8,21 +8,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // GL 需要2个必要组件：GLSurfaceView视图 和 GLSurfaceView.Renderer渲染器
-    // GL view needed to draw things on.
-    private GLSurfaceView mGLSurfaceView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mGLSurfaceView = new GLSurfaceView(this);
+        // GL 需要2个必要组件：GLSurfaceView视图 和 GLSurfaceView.Renderer渲染器
+        // GL view needed to draw things on.
+        GLSurfaceView mGLSurfaceView = new GLSurfaceView(this);
         setContentView(mGLSurfaceView);
         //设置版本
         mGLSurfaceView.setEGLContextClientVersion(3);
 
         //创建渲染器
-        GLSurfaceView.Renderer renderer = new ColorRenderer(Color.GRAY);
+        //GLSurfaceView.Renderer renderer = new ColorRenderer(Color.GRAY);
+        GLSurfaceView.Renderer renderer = new TriangleRenderer();
         //GLSurfaceView.Renderer renderer = new NativeColorRenderer(Color.GRAY);
         mGLSurfaceView.setRenderer(renderer);
     }
